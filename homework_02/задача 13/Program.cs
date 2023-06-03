da13266d2c01402d65acd7e0628ec1ef161a9b39
Northbreez <chesden84@mail.ru>
2023-06-03 11:28:03 +0300

    Решение задачи 13

---
+++ homework_02/задача 13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Через строку решать нельзя.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите любое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if  (num < 99)
+    System.Console.WriteLine("третьей цифры нет");
+else if  (num >= 100)
+    while(num > 999)
+    {
+    num = num / 10;
+    }
+    System.Console.WriteLine(num % 10);
