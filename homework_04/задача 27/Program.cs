36c7e893697403bb2ea79098f17eab10b7b49e2c
Northbreez <chesden84@mail.ru>
2023-06-03 16:14:07 +0300

    Решение задачи 27

---
+++ homework_04/задача 27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+return result;
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите любое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма цифр цисла {num} равнa - {Sum(num)}");
