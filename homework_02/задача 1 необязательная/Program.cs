2d2c6c04e95c52ee8d18cb05295e9c17dc045198
Northbreez <chesden84@mail.ru>
2023-06-03 10:50:09 +0300

    Решение задачи 1 необязательной

---
+++ homework_02/задача 1 необязательная/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача необязательная 1: на входе целое или вещественное число, надо удалить вторую цифру слева этого 
+//числа.
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+
+ Console.WriteLine("Введите любое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num < 100) || (num > 999))
+    Console.WriteLine("Нет");
+else
+{
+int x1 = num / 100;
+int x2 = num % 10;
+Console.WriteLine($"{x1}{x2}");
+}
