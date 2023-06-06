14ab99f2e6bacfae3a5d8c7fcfb41ad2b87d8745
Northbreez <chesden84@mail.ru>
2023-06-06 20:43:22 +0300

    Решение задачи 38

---
+++ homework_05/задача 36/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//  Найдите сумму элементов, стоящих на нечётных индексах.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(10,99);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+void SumNechetyIndex(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length;)
+    {
+        sum += array[i];
+        i += 2;
+    }
+    System.Console.WriteLine(sum);
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите размерность массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+SumNechetyIndex(array);
