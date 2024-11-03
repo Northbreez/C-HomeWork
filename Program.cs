21428833b896a3c0b957a9c6a5f8ecd684f020ac
Northbreez <Chesden84@mail.ru>
2024-11-03 14:39:23 +0300

    Создали функцию заполнения второго массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array1 = new string[6] {"geek", "234", "brains", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
+void FillSecondArray(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[j] = array1[i];
+        j++;
+        }
+    }
+}
