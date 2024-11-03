8bd1cba5465dee6529d1e434582249091075097c
Northbreez <Chesden84@mail.ru>
2024-11-03 14:28:05 +0300

    Создали два массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[6] {"geek", "234", "brains", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
